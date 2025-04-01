a2fc8578c64ef1a1b02dd825b617adde679ebefa
Sahil7015 <sahil94855@gmail.com>
2025-04-01 21:45:56 +0530

    portfolio

---
+++ src/components/About/About.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            {/* <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" /> */}
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+              Hi, I'm Sahil, a front-end web developer and a BE graduate with a strong passion for building user-friendly, responsive web applications With a keen eye for detail and a deep understanding of modern web technologies. I excels at creating seamless user experiences. I am proficient in HTML, CSS, JavaScript, and React, and has experience developing scalable, intuitive interfaces that enhance both usability and performance. I am committed to continuous learning and staying updated with the latest trends in front-end development to deliver high-quality solutions
+              </p>
+            </div>
+          </li>
+          {/* <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience developing fast and optimised back-end systems
+                and APIs
+              </p>
+            </div>
+          </li> */}
+          {/* <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>
+                I have designed multiple landing pages and have created design
+                systems as well
+              </p>
+            </div>
+          </li> */}
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Sahil</h1>
+        <p className={styles.description}>
+        A front-end web developer specializes in designing and building user interfaces using HTML, CSS, and React js to create seamless and interactive web experiences
+        </p>
+        <a href="sahil94855@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
